5154d48e98298d49ef54a2b2c3f135b519a1388d
bookerubk <booker.ubk@gmail.com>
2024-09-13 09:09:20 +0300

    Update and rename index.py to main.py

---
+++ api/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, BackgroundTasks
+from telegram_bot import process_update, bot
+from models import TelegramWebhook
+
+# Инициализируем FastAPI
+app = FastAPI()
+
+# Маршрут для обработки вебхуков от Telegram
+@app.post("/webhook")
+async def telegram_webhook(update: TelegramWebhook, background_tasks: BackgroundTasks):
+    # Передаем обновление в фоновую задачу для обработки
+    background_tasks.add_task(process_update, update.dict())
+    return {"status": "ok"}
+
+# Маршрут для установки вебхука
+@app.get("/set_webhook")
+async def set_webhook():
+    webhook_url = "https://tg-bot-git-main-bookerubks-projects.vercel.app/webhook"  # Укажите ваш URL для вебхука
+    success = bot.set_webhook(webhook_url)
+    if success:
+        return {"message": "Webhook установлен"}
+    else:
+        return {"message": "Не удалось установить webhook"}
+
+# Маршрут для удаления вебхука
+@app.get("/delete_webhook")
+async def del_webhook():
+    success = bot.delete_webhook()
+    if success:
+        return {"message": "Webhook удален"}
+    else:
+        return {"message": "Не удалось удалить webhook"}
